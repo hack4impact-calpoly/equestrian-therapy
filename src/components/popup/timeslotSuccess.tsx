6a5af8902b426bb61bc80de2569feb7d054bcc8e
Hayley <haysho2260@gmail.com>
2023-05-02 17:58:58 -0700

    code look work. code no work. hayley is done

--- src/components/popup/timeslotSuccess.tsx
+++ src/components/popup/timeslotSuccess.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { DataStore } from "aws-amplify";
 import logo from "../../images/horseRider.svg";
+import { Timeslot } from "../../models";
 import {
   Wrapper,
   Box,
@@ -9,6 +11,44 @@ import {
   CenteredDescription,
   CenteredHeader,
 } from "../styledComponents";
+
+async function updatePost(
+  id: string,
+  unavailableDate: (string | null)[] | null | undefined
+) {
+  // unavailabledate is of form mm/dd/yyyy
+  try {
+    const original = await DataStore.query(Timeslot, id);
+    if (
+      original !== null &&
+      original !== undefined &&
+      Array.isArray(original.unavailableDates) &&
+      Array.isArray(unavailableDate)
+    ) {
+      const updatedList = [...original.unavailableDates, ...unavailableDate];
+      if (original && updatedList) {
+        const updatedPost = await DataStore.save(
+          Timeslot.copyOf(original, (updated) => {
+            updated.unavailableDates = updatedList;
+          })
+        );
+        console.log("updated post", updatedPost);
+      }
+    } else if (original) {
+      if (original && Array.isArray(unavailableDate)) {
+        const updatedPost = await DataStore.save(
+          Timeslot.copyOf(original, (updated) => {
+            updated.unavailableDates = unavailableDate;
+          })
+        );
+        console.log("updated post", updatedPost);
+      }
+    }
+  } catch {
+    console.log("An error occurred: ", Error);
+  }
+  console.log("updated timeslot", await DataStore.query(Timeslot, id));
+}
 
 const Logo = styled.img`
   width: 2.5em;
@@ -22,6 +62,7 @@ export default function timeslotSuccess() {
 
   const handleClick = () => {
     navigate("/");
+    updatePost("89bf61ec-5b7d-44a0-8c0b-4e8321d9cc2a", ["05/02/2023"]);
   };
 
   return (
