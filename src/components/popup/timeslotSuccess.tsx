ceb6e8443e1323499214b8ec3c27390777733ff0
Parshana007 <parshana007@gmail.com>
2023-05-05 12:33:01 -0700

    Code works; with console debug messages

--- src/components/popup/timeslotSuccess.tsx
+++ src/components/popup/timeslotSuccess.tsx
@@ -4,15 +4,6 @@ import styled from "styled-components";
 import { DataStore } from "aws-amplify";
 import logo from "../../images/horseRider.svg";
 import { Booking, User } from "../../models";
-
-/* 
-check booking user type
-if volunteer, allow time of any length
-if rider, only allow list of length 1
-add to booking instead of timeslot
-be able to search and see if the specific time has been bokked already
-*/
-
 import {
   Wrapper,
   Box,
@@ -45,15 +36,19 @@ async function addRVBooking(
         const isoDate = new Date(bookedDates[i]).toISOString().split("T")[0];
         const descriptionStr: string = `User: ${userID} Booked Time: ${isoDate}`;
         const booking = new Booking({
-          title: "New Booking",
+          title: "New Booking -- Volunteer",
           date: isoDate,
           description: descriptionStr,
           timeslotID: TimslotID,
           userID,
         });
         promises.push(DataStore.save(booking));
+        // const savedBooking = await DataStore.save(booking);
+        // const savedBookingId = savedBooking.id;
       }
       await Promise.all(promises);
+
+      // console.log("Add booking", await Promise.all(promises));
     } else if (
       original !== null &&
       original !== undefined &&
@@ -70,6 +65,12 @@ async function addRVBooking(
           userID,
         });
         await DataStore.save(booking);
+        // const savedBooking = await DataStore.save(booking);
+        // const savedBookingId = savedBooking.id;
+        // console.log(
+        //   "Add booking",
+        //   await DataStore.query(Booking, savedBookingId)
+        // );
       }
     }
   } catch (error: unknown) {
@@ -77,7 +78,6 @@ async function addRVBooking(
       console.log("An error occurred: ", error.message);
     }
   }
-  console.log("Add booking", await DataStore.query(Booking, TimslotID));
 }
 
 // async function deleteUnavailability(id: string, availableDate: string[]) {
@@ -101,7 +101,8 @@ export default function timeslotSuccess() {
   const navigate = useNavigate();
   addRVBooking(
     "5dc2eecb-89bc-4bbf-937c-2ab0ddbd3671",
-    "d64640ae-b8ef-4485-96a3-013931d15a5d",
+    "5bfff0a7-42aa-48f7-bccb-0fa60dd0b6d3",
+    // ["2023/05/05"]
     ["2023/05/05", "2023/05/06", "2023/05/07"]
   );
 
