da8849dc184c122ee3b63449f09e8671f3a5bf0c
Hayley <haysho2260@gmail.com>
2023-05-07 13:38:29 -0700

    popup works, not css

--- src/components/popup/timeslotConfirmation.tsx
+++ src/components/popup/timeslotConfirmation.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import warning from "../../images/warning.svg";
 
 import {
   Wrapper,
+  PopupDiv,
   Box,
   Description,
   Header,
@@ -38,55 +39,71 @@ const CancelButton = styled(Button)`
   color: #1b4c5a;
   margin-right: 1rem;
 `;
+const TempButton = styled.button`
+  position: absolute;
+  top: 10%;
+  left: 40%;
+`;
 
 export default function TimeSlotConfirmation({
   userType,
   status = "",
 }: TimeSlotProps) {
+  const [open, setOpen] = useState(false);
   return (
     <Wrapper>
-      {userType === "admin" && (
+      <TempButton onClick={() => setOpen(true)}>Open popup</TempButton>
+      <PopupDiv
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
         <SurroundingBox>
-          <Warning src={warning} />
-          <Header>Save changes?</Header>
-          <Description>
-            You are choosing to edit the availability of one or more time slots.
-            Are you sure you want to do this?
-          </Description>
-          <Row>
-            <CancelButton>Cancel</CancelButton>
-            <ConfirmButton>Confirm</ConfirmButton>
-          </Row>
+          {userType === "admin" && (
+            <SurroundingBox>
+              <Warning src={warning} />
+              <Header>Save changes?</Header>
+              <Description>
+                You are choosing to edit the availability of one or more time
+                slots. Are you sure you want to do this?
+              </Description>
+              <Row>
+                <CancelButton>Cancel</CancelButton>
+                <ConfirmButton>Confirm</ConfirmButton>
+              </Row>
+            </SurroundingBox>
+          )}
+          {userType !== "admin" && status === "cancel" && (
+            <SurroundingBox>
+              <Warning src={warning} />
+              <Header>Confirm cancellation?</Header>
+              <Description>
+                You are choosing to cancel one or more time slots. Are you sure
+                you want to do this?
+              </Description>
+              <Row>
+                <CancelButton>Cancel</CancelButton>
+                <ConfirmButton>Confirm</ConfirmButton>
+              </Row>
+            </SurroundingBox>
+          )}
+          {userType !== "admin" && status === "book" && (
+            <SurroundingBox>
+              <Warning src={warning} />
+              <Header>Confirm booking?</Header>
+              <Description>
+                You are choosing to book one or more time slots. Are you sure
+                you want to do this?
+              </Description>
+              <Row>
+                <CancelButton>Cancel</CancelButton>
+                <ConfirmButton>Book</ConfirmButton>
+              </Row>
+            </SurroundingBox>
+          )}
         </SurroundingBox>
-      )}
-      {userType !== "admin" && status === "cancel" && (
-        <SurroundingBox>
-          <Warning src={warning} />
-          <Header>Confirm cancellation?</Header>
-          <Description>
-            You are choosing to cancel one or more time slots. Are you sure you
-            want to do this?
-          </Description>
-          <Row>
-            <CancelButton>Cancel</CancelButton>
-            <ConfirmButton>Confirm</ConfirmButton>
-          </Row>
-        </SurroundingBox>
-      )}
-      {userType !== "admin" && status === "book" && (
-        <SurroundingBox>
-          <Warning src={warning} />
-          <Header>Confirm booking?</Header>
-          <Description>
-            You are choosing to book one or more time slots. Are you sure you
-            want to do this?
-          </Description>
-          <Row>
-            <CancelButton>Cancel</CancelButton>
-            <ConfirmButton>Book</ConfirmButton>
-          </Row>
-        </SurroundingBox>
-      )}
+      </PopupDiv>
     </Wrapper>
   );
 }
