d2e6266900a28eeb22f0b605750684a6e330c56a
Parshana007 <parshana007@gmail.com>
2023-05-11 15:34:39 -0700

    ;( moved code from success to confirmatinon :) yay

--- src/components/popup/timeslotConfirmation.tsx
+++ src/components/popup/timeslotConfirmation.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { DataStore } from "aws-amplify";
 import warning from "../../images/warning.svg";
+import { Timeslot, User, Booking } from "../../models";
 
 import {
   Wrapper,
@@ -39,10 +42,155 @@ const CancelButton = styled(Button)`
   margin-right: 1rem;
 `;
 
+async function addUnavailability(id: string, unavailableDate: string[]) {
+  try {
+    const original = await DataStore.query(Timeslot, id);
+    if (
+      original !== null &&
+      original !== undefined &&
+      Array.isArray(original.unavailableDates) &&
+      Array.isArray(unavailableDate)
+    ) {
+      const updatedList = new Set(original.unavailableDates);
+      const isoDates = new Set(
+        unavailableDate.map((dateString) => {
+          const date = new Date(dateString);
+          return date.toISOString().split("T")[0];
+        })
+      );
+      isoDates.forEach((isoDate) => {
+        updatedList.add(isoDate);
+      });
+      await DataStore.save(
+        Timeslot.copyOf(original, (updated) => {
+          updated.unavailableDates = Array.from(updatedList); // eslint-disable-line no-param-reassign
+        })
+      );
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log("An error occurred: ", error.message);
+    }
+  }
+  console.log("Add unavailable times", await DataStore.query(Timeslot, id));
+}
+
+async function deleteUnavailability(id: string, availableDate: string[]) {
+  try {
+    const original = await DataStore.query(Timeslot, id);
+    if (
+      original !== null &&
+      original !== undefined &&
+      Array.isArray(original.unavailableDates)
+    ) {
+      const isoDates = availableDate.map((dateString) => {
+        const date = new Date(dateString);
+        return date.toISOString().split("T")[0];
+      });
+      const updatedList = original.unavailableDates.filter((dateString) => {
+        if (dateString !== null) {
+          const isoDate = new Date(dateString).toISOString().split("T")[0];
+          return !isoDates.includes(isoDate);
+        }
+        return false;
+      });
+      await DataStore.save(
+        Timeslot.copyOf(original, (updated) => {
+          updated.unavailableDates = updatedList; // eslint-disable-line no-param-reassign
+        })
+      );
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log("An error occurred: ", error.message);
+    }
+  }
+
+  console.log("Add available times", await DataStore.query(Timeslot, id)); // eslint-disable-line no-param-reassign
+}
+
+async function addRVBooking(
+  TimslotID: string,
+  userID: string,
+  bookedDates: string[]
+) {
+  try {
+    const original = await DataStore.query(User, userID);
+    if (
+      original !== null &&
+      original !== undefined &&
+      original.userType === "Volunteer"
+    ) {
+      const promises = [];
+      for (let i = 0; i < bookedDates.length; i++) {
+        const isoDate = new Date(bookedDates[i]).toISOString().split("T")[0];
+        const descriptionStr: string = `User: ${userID} Booked Time: ${isoDate}`;
+        const booking = new Booking({
+          title: "New Booking -- Volunteer",
+          date: isoDate,
+          description: descriptionStr,
+          timeslotID: TimslotID,
+          userID,
+        });
+        promises.push(DataStore.save(booking));
+      }
+      await Promise.all(promises);
+    } else if (
+      original !== null &&
+      original !== undefined &&
+      original.userType === "Rider"
+    ) {
+      if (bookedDates.length === 1) {
+        const isoDate = new Date(bookedDates[0]).toISOString().split("T")[0];
+        const descriptionStr: string = `User: ${userID} Booked Time: ${isoDate}`;
+        const booking = new Booking({
+          title: "New Booking",
+          date: isoDate,
+          description: descriptionStr,
+          timeslotID: TimslotID,
+          userID,
+        });
+        await DataStore.save(booking);
+      }
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log("An error occurred: ", error.message);
+    }
+  }
+}
+
 export default function TimeSlotConfirmation({
   userType,
   status = "",
 }: TimeSlotProps) {
+  const navigate = useNavigate();
+
+  const handleConfirmationAdmin = () => {
+    navigate("/timeslot-success");
+    addUnavailability("5dc2eecb-89bc-4bbf-937c-2ab0ddbd3671", [
+      "2023/05/02",
+      "2023/05/03",
+    ]); // YYYY-MM-DD
+    deleteUnavailability("5dc2eecb-89bc-4bbf-937c-2ab0ddbd3671", [
+      "2023/05/02",
+    ]); // YYYY-MM-DD
+  };
+
+  const handleConfirmationRV = () => {
+    navigate("/timeslot-success");
+    addRVBooking(
+      "5dc2eecb-89bc-4bbf-937c-2ab0ddbd3671",
+      "5bfff0a7-42aa-48f7-bccb-0fa60dd0b6d3",
+      // ["2023/05/05"]
+      ["2023/05/05", "2023/05/06", "2023/05/07"]
+    );
+  };
+
+  const handleCancel = () => {
+    navigate("/");
+  };
+
   return (
     <Wrapper>
       {userType === "admin" && (
@@ -54,7 +202,7 @@ export default function TimeSlotConfirmation({
             Are you sure you want to do this?
           </Description>
           <Row>
-            <CancelButton>Cancel</CancelButton>
+            <CancelButton onClick={handleCancel}>Cancel</CancelButton>
             <ConfirmButton>Confirm</ConfirmButton>
           </Row>
         </SurroundingBox>
@@ -68,7 +216,7 @@ export default function TimeSlotConfirmation({
             want to do this?
           </Description>
           <Row>
-            <CancelButton>Cancel</CancelButton>
+            <CancelButton onClick={handleCancel}>Cancel</CancelButton>
             <ConfirmButton>Confirm</ConfirmButton>
           </Row>
         </SurroundingBox>
@@ -82,8 +230,8 @@ export default function TimeSlotConfirmation({
             want to do this?
           </Description>
           <Row>
-            <CancelButton>Cancel</CancelButton>
-            <ConfirmButton>Book</ConfirmButton>
+            <CancelButton onClick={handleCancel}>Cancel</CancelButton>
+            <ConfirmButton onClick={handleConfirmationRV}>Book</ConfirmButton>
           </Row>
         </SurroundingBox>
       )}
--- src/components/popup/timeslotSuccess.tsx
+++ src/components/popup/timeslotSuccess.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { DataStore } from "aws-amplify";
-// import AWSDate from "@aws-amplify/core";
 import logo from "../../images/horseRider.svg";
-import { Timeslot } from "../../models";
 import {
   Wrapper,
   Box,
@@ -12,73 +9,6 @@ import {
   CenteredDescription,
   CenteredHeader,
 } from "../styledComponents";
-
-async function addUnavailability(id: string, unavailableDate: string[]) {
-  try {
-    const original = await DataStore.query(Timeslot, id);
-    if (
-      original !== null &&
-      original !== undefined &&
-      Array.isArray(original.unavailableDates) &&
-      Array.isArray(unavailableDate)
-    ) {
-      const updatedList = new Set(original.unavailableDates);
-      const isoDates = new Set(
-        unavailableDate.map((dateString) => {
-          const date = new Date(dateString);
-          return date.toISOString().split("T")[0];
-        })
-      );
-      isoDates.forEach((isoDate) => {
-        updatedList.add(isoDate);
-      });
-      await DataStore.save(
-        Timeslot.copyOf(original, (updated) => {
-          updated.unavailableDates = Array.from(updatedList); // eslint-disable-line no-param-reassign
-        })
-      );
-    }
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log("An error occurred: ", error.message);
-    }
-  }
-  console.log("Add unavailable times", await DataStore.query(Timeslot, id));
-}
-
-async function deleteUnavailability(id: string, availableDate: string[]) {
-  try {
-    const original = await DataStore.query(Timeslot, id);
-    if (
-      original !== null &&
-      original !== undefined &&
-      Array.isArray(original.unavailableDates)
-    ) {
-      const isoDates = availableDate.map((dateString) => {
-        const date = new Date(dateString);
-        return date.toISOString().split("T")[0];
-      });
-      const updatedList = original.unavailableDates.filter((dateString) => {
-        if (dateString !== null) {
-          const isoDate = new Date(dateString).toISOString().split("T")[0];
-          return !isoDates.includes(isoDate);
-        }
-        return false;
-      });
-      await DataStore.save(
-        Timeslot.copyOf(original, (updated) => {
-          updated.unavailableDates = updatedList; // eslint-disable-line no-param-reassign
-        })
-      );
-    }
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log("An error occurred: ", error.message);
-    }
-  }
-
-  console.log("Add available times", await DataStore.query(Timeslot, id)); // eslint-disable-line no-param-reassign
-}
 
 const Logo = styled.img`
   width: 2.5em;
@@ -89,15 +19,9 @@ const Logo = styled.img`
 
 export default function timeslotSuccess() {
   const navigate = useNavigate();
+
   const handleClick = () => {
     navigate("/");
-    addUnavailability("5dc2eecb-89bc-4bbf-937c-2ab0ddbd3671", [
-      "2023/05/02",
-      "2023/05/03",
-    ]); // YYYY-MM-DD
-    deleteUnavailability("5dc2eecb-89bc-4bbf-937c-2ab0ddbd3671", [
-      "2023/05/02",
-    ]); // YYYY-MM-DD
   };
 
   return (
