03ec1fb83c61f765d60d543dcfa660249198b395
borajoo <chocobora1@gmail.com>
2023-07-30 18:28:53 -0700

    Admins can enable Sunday timeslots

--- src/components/popup/timeslotConfirmation.tsx
+++ src/components/popup/timeslotConfirmation.tsx
@@ -68,17 +68,32 @@ export default function TimeSlotConfirmation({
     try {
       ids.forEach(async (timeslotId) => {
         const original = await DataStore.query(Timeslot, timeslotId);
-        if (original && Array.isArray(original.unavailableDates)) {
+        if (
+          original &&
+          Array.isArray(original.unavailableDates) &&
+          Array.isArray(original.availableSundays)
+        ) {
           const ymdDate = convertToYMD(new Date(unavailableDate));
-          const updatedList = new Set(original.unavailableDates);
-          if (!updatedList.has(ymdDate)) {
-            updatedList.add(ymdDate);
+          if (unavailableDate.getDay() === 0) {
+            const updatedList = original.availableSundays.filter(
+              (dateString) => ymdDate !== dateString
+            );
             await DataStore.save(
               Timeslot.copyOf(original, (updated) => {
-                // eslint-disable-next-line no-param-reassign
-                updated.unavailableDates = Array.from(updatedList);
+                updated.availableSundays = updatedList; // eslint-disable-line no-param-reassign
               })
             );
+          } else {
+            const updatedList = new Set(original.unavailableDates);
+            if (!updatedList.has(ymdDate)) {
+              updatedList.add(ymdDate);
+              await DataStore.save(
+                Timeslot.copyOf(original, (updated) => {
+                  // eslint-disable-next-line no-param-reassign
+                  updated.unavailableDates = Array.from(updatedList);
+                })
+              );
+            }
           }
         }
       });
@@ -93,16 +108,33 @@ export default function TimeSlotConfirmation({
     try {
       ids.forEach(async (timeslotId) => {
         const original = await DataStore.query(Timeslot, timeslotId);
-        if (original && Array.isArray(original.unavailableDates)) {
+        if (
+          original &&
+          Array.isArray(original.unavailableDates) &&
+          Array.isArray(original.availableSundays)
+        ) {
           const convertedDate = convertToYMD(new Date(availableDate));
-          const updatedList = original.unavailableDates.filter(
-            (dateString) => convertedDate !== dateString
-          );
-          await DataStore.save(
-            Timeslot.copyOf(original, (updated) => {
-              updated.unavailableDates = updatedList; // eslint-disable-line no-param-reassign
-            })
-          );
+          if (availableDate.getDay() === 0) {
+            const updatedList = new Set(original.availableSundays);
+            if (!updatedList.has(convertedDate)) {
+              updatedList.add(convertedDate);
+              await DataStore.save(
+                Timeslot.copyOf(original, (updated) => {
+                  // eslint-disable-next-line no-param-reassign
+                  updated.availableSundays = Array.from(updatedList);
+                })
+              );
+            }
+          } else {
+            const updatedList = original.unavailableDates.filter(
+              (dateString) => convertedDate !== dateString
+            );
+            await DataStore.save(
+              Timeslot.copyOf(original, (updated) => {
+                updated.unavailableDates = updatedList; // eslint-disable-line no-param-reassign
+              })
+            );
+          }
         }
       });
     } catch (error: unknown) {
