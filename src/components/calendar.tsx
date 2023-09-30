fbee4f29af04f90cd30b85b16e0806f5e737c781
borajoo <chocobora1@gmail.com>
2023-09-29 21:54:42 -0700

    Update PET logo name

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -12,7 +12,7 @@ import interactionPlugin from "@fullcalendar/interaction";
 import UserContext from "../userContext";
 import { LazyTimeslot } from "../models";
 // import Monthly from "./monthlyView";
-import logo from "../images/petLogo2.svg";
+import logo from "../images/petLogo.svg";
 import Toggle from "./calendarToggle";
 import Popup from "./popup/timeslotPopup";
 // import FullCalendar from "@fullcalendar/react";
